16248a675c9e1812f6148419c8af72f854e7565b
itsramoune <48112421+itsramoune@users.noreply.github.com>
2019-06-19 12:06:55 -0600

    Add files via upload

--- js/my-app.js
+++ js/my-app.js
@@ -16,95 +16,33 @@ var myApp = new Framework7({
     	url: 'index.html',
     	name: 'home',
   		},
-		
-		
 		{
-		path: '/perfil/',
-    	url: 'perfil.html',
-    	name: 'perfil',
+		path: '/artist/',
+    	url: 'artist.html',
+    	name: 'artist',
   		},
-		
-		{
-		path: '/calendario/',
-    	url: 'calendario.html',
-    	name: 'calendario',
-  		},
-
-		
-		{
-		path: '/form/',
-    	url: 'form.html',
-    	name: 'form',
-  		},
-		
-	
 		{
 		path: '/list/',
     	url: 'list.html',
     	name: 'list',
   		},
-		
-		
-		
-		{
-		path: '/menu/',
-    	url: 'menu.html',
-    	name: 'menu',
-  		},
-		
 		{
 		path: '/info/',
     	url: 'info.html',
     	name: 'info',
   		},
-		
 		{
-		path: '/adopta/',
-    	url: 'adopta.html',
-    	name: 'adopta',
+		path: '/radio/',
+    	url: 'radio.html',
+    	name: 'radio',
   		},
-		
-		
-		
 		{
-		path: '/about/',
-    	url: 'about.html',
-    	name: 'about',
+		path: '/profile/',
+    	url: 'profile.html',
+    	name: 'profile',
   		},
-		
-		
-		{
-		path: '/rastrear/',
-    	url: 'rastrear.html',
-    	name: 'rastrear',
-  		},
-		
-		{
-		path: '/lugares/',
-    	url: 'lugares.html',
-    	name: 'lugares',
-  		},
-		
-		{
-		path: '/veterinario/',
-    	url: 'veterinario.html',
-    	name: 'veterinario',
-  		},
-		
-		{
-		path: '/actividades/',
-    	url: 'actividades.html',
-    	name: 'veterinario',
-  		},
-		
-		{
-		path: '/chat/',
-    	url: 'chat.html',
-    	name: 'chat',
-  		},
-		
-		
-		
+	
+	
 		
 	]
 });
